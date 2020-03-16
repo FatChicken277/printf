f60ee426a7c705dbcf8d074b9982a1aeed5fd7d9
clasesucatmarlon <clasesucatmarlon@github.com>
2020-03-16 16:10:26 +0000

    Added all files with posterior corrections

--- _printf.c
+++ _printf.c
@@ -1,69 +1,35 @@
 #include "holberton.h"
 #include <stdio.h>
 
+/**
+ * _printf - Print string
+ * @format: arguments
+ * Return: return the amount of characters
+ */
+
+
 int _printf(const char *format, ...)
 {
-    int i = 0, j = 0, a = 0, k = 0;
-    int p;
-    char *s;
-    unsigned int u;
-    va_list list;
-    va_list list2;
-    va_list list3;
 
-    va_start(list, format);
-    va_start(list2, format);
-    va_start(list3, format);
+	op_t ops[] = {
+		{"c", _print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{NULL, NULL}};
 
-        while (format[i] != '\0')
-        {
-            if (format[i] == '%' && format[i+1] == '%')
-                a--;
-            i++;
-        }
-        a += i;
+	int print = 0;
+	va_list ap;
 
-        p = va_arg(list, int);
-        s = va_arg(list2, char *);
-        u = va_arg(list3, unsigned int);
-        for (j = 0; j < i; j++)
-        {
-            if ((format[j] == '%') && (format[j+1] == 'd' || format[j+1] == 'i'))
-            {
-                print_number(p);
-                j++;
-            }
-            else if (format[j] == '%' && format[j+1] == '%')
-                ;
-            else if (format[j] == '%' && format[j+1] == 'c')
-            {
-                _putchar(p);
-                j++;
-            }
-            else if (format[j] == '%' && format[j+1] == 's')
-            {
-                for (k = 0; s[k] != '\0'; k++)
-                {
-                    _putchar(s[k]);
-                }
-                j++;
-            }
-            else if (format[j] == '%' && format[j+1] == 'u')
-            {
-                unsig(u);
-                j++;
-            }
-            else if (format[j] == '%' && format[j+1] == 'o')
-            {
-                octal(p);
-                j++;
-            }
-            else
-                _putchar(format[j]);
-        }
+	if (format != NULL)
+	{
+		va_start(ap, format);
+		print = _parseo(format, ops, ap);
+		va_end(ap);
+	}
+	else
+	{
+		return (-1);
+	}
 
-    va_end(list);
-    va_end(list2);
-    va_end(list3);
-    return (a);
+	return (print);
 }
--- holberton.h
+++ holberton.h
@@ -1,11 +1,29 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 #include <stdarg.h>
+#include <stdlib.h>
 #include <unistd.h>
-/**  printf **/
+
+/**  Function printf **/
+/**********************/
+
+/**
+ * struct op - structure to use in printf
+ * @op: ooperator of structure
+ * @f: function of work
+ */
+
+typedef struct op
+{
+	char *op;
+	int (*f)(va_list);
+} op_t;
+
+/* Prototype function to printf */
 int _printf(const char *format, ...);
-int _putchar(char c);
-void print_number(int n);
-void unsig(unsigned int n);
-void octal(unsigned int n);
+int print_char(char c);
+int _print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+int _parseo(const char *format, op_t ops[], va_list ap);
 #endif
