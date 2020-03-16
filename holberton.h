5cf9ec3ade4860793e3b40867285b9eb7664d381
noroot <noroot@localhost.localdomain>
2020-03-16 12:57:09 -0500

    Update to the protoypes functions

--- holberton.h
+++ holberton.h
@@ -3,27 +3,30 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+ 
 /**  Function printf **/
 /**********************/
-
+ 
 /**
  * struct op - structure to use in printf
  * @op: ooperator of structure
  * @f: function of work
  */
-
+ 
 typedef struct op
 {
-	char *op;
-	int (*f)(va_list);
+    char *op;
+    int (*f)(va_list);
 } op_t;
-
+ 
 /* Prototype function to printf */
 int _printf(const char *format, ...);
 int print_char(char c);
 int _print_char(va_list);
 int print_string(va_list);
 int print_percent(va_list);
+int print_integer(va_list ap);
+void print_number(int n);
+ 
 int _parseo(const char *format, op_t ops[], va_list ap);
 #endif
