16ba6e25e5a92444d84ff03499452651d3976542
clasesucatmarlon <clasesucatmarlon@github.com>
2020-03-17 16:46:39 +0000

    Change to print_char.c

--- print_char.c
+++ print_char.c
@@ -3,7 +3,7 @@
 
 /**
  * print_char - writes the character c to stdout
- * @c: The character to print
+ * @c: The character to print input
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
